8fb3c5b967d95365bc190e79c8efd9c9f0a137b9
Lucas-Vergara <lucasvergara1@gmail.com>
2024-09-13 09:38:46 -0300

    Refactor distributor list in scraping service and update MongoDB connection string in app.module.ts

--- src/modules/scraping/services/scraping.service.ts
+++ src/modules/scraping/services/scraping.service.ts
@@ -29,16 +29,16 @@ export class ScrapingService {
       let productsAmount = 0;
 
       const distributors = [
-        // { name: 'Construmart', products: filterProductsByDistributor(products, 'Construmart'), priceSelector: '.vtex-product-price-1-x-sellingPriceValue', titleSelector: '.vtex-store-components-3-x-productBrand' },
-        // { name: 'Construplaza', products: filterProductsByDistributor(products, 'Construplaza'), priceSelector: 'span.price', titleSelector: 'div.name' },
+        { name: 'Construmart', products: filterProductsByDistributor(products, 'Construmart'), priceSelector: '.vtex-product-price-1-x-sellingPriceValue', titleSelector: '.vtex-store-components-3-x-productBrand' },
+        { name: 'Construplaza', products: filterProductsByDistributor(products, 'Construplaza'), priceSelector: 'span.price', titleSelector: 'div.name' },
         { name: 'Easy', products: filterProductsByDistributor(products, 'Easy'), priceSelector: 'div.easycl-precio-cencosud-0-x-lastPrice', titleSelector: 'span.vtex-store-components-3-x-productBrand' },
-        // { name: 'Ferrobal', products: filterProductsByDistributor(products, 'Ferrobal'), priceSelector: '', titleSelector: '', },
-        // { name: 'Homecenter', products: filterProductsByDistributor(products, 'Sodimac'), priceSelector: '', titleSelector: '' },
-        // { name: 'Imperial', products: filterProductsByDistributor(products, 'Imperial'), priceSelector: '#root > main > section:nth-child(2) > div > div > section > div.osf__sc-d9hb4p-0.osf__sc-ezuj69-0.jRBkAY.hrAxGa > div > div.osf__sc-d9hb4p-0.osf__sc-1wibf4t-0.osf__sc-1kvhwj2-0.lnkVmY.hzGYQn.lfmKWP > div > p', titleSelector: '#root > main > section:nth-child(2) > div > div > section > div.osf__sc-d9hb4p-0.osf__sc-ezuj69-0.jRBkAY.hrAxGa > div > div.osf__sc-d9hb4p-0.osf__sc-1wibf4t-0.osf__sc-7hvb28-0.jRBkAY.hzGYQn.cowjsN > h2 > span' },
-        // { name: 'Prodalam', products: filterProductsByDistributor(products, 'Prodalam'), priceSelector: 'span#gtm_price', titleSelector: 'div.detail__title' },
-        // { name: 'Toso', products: filterProductsByDistributor(products, 'Toso'), priceSelector: '', titleSelector: '' },
-        // { name: 'Weitzler', products: filterProductsByDistributor(products, 'Weitzler'), priceSelector: 'p.price.product-page-price', titleSelector: 'h1.product-title.product_title.entry-title' },
-        // { name: 'Yolito', products: filterProductsByDistributor(products, 'Yolito'), priceSelector: '', titleSelector: '' },
+        { name: 'Ferrobal', products: filterProductsByDistributor(products, 'Ferrobal'), priceSelector: '', titleSelector: '', },
+        { name: 'Homecenter', products: filterProductsByDistributor(products, 'Sodimac'), priceSelector: '', titleSelector: '' },
+        { name: 'Imperial', products: filterProductsByDistributor(products, 'Imperial'), priceSelector: '#root > main > section:nth-child(2) > div > div > section > div.osf__sc-d9hb4p-0.osf__sc-ezuj69-0.jRBkAY.hrAxGa > div > div.osf__sc-d9hb4p-0.osf__sc-1wibf4t-0.osf__sc-1kvhwj2-0.lnkVmY.hzGYQn.lfmKWP > div > p', titleSelector: '#root > main > section:nth-child(2) > div > div > section > div.osf__sc-d9hb4p-0.osf__sc-ezuj69-0.jRBkAY.hrAxGa > div > div.osf__sc-d9hb4p-0.osf__sc-1wibf4t-0.osf__sc-7hvb28-0.jRBkAY.hzGYQn.cowjsN > h2 > span' },
+        { name: 'Prodalam', products: filterProductsByDistributor(products, 'Prodalam'), priceSelector: 'span#gtm_price', titleSelector: 'div.detail__title' },
+        { name: 'Toso', products: filterProductsByDistributor(products, 'Toso'), priceSelector: '', titleSelector: '' },
+        { name: 'Weitzler', products: filterProductsByDistributor(products, 'Weitzler'), priceSelector: 'p.price.product-page-price', titleSelector: 'h1.product-title.product_title.entry-title' },
+        { name: 'Yolito', products: filterProductsByDistributor(products, 'Yolito'), priceSelector: '', titleSelector: '' },
       ];
 
       for (const distributor of distributors) {
